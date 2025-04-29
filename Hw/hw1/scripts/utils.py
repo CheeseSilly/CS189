1aab3bfea11965113abf8b583bce904f97078f65
HazardWen <si11ycheese6680@gmail.com>
2025-04-29 12:27:24 +0800

    Delete Hw/hw1 directory

--- Hw/hw1/scripts/utils.py
+++
@@ -1,8 +0,0 @@
-import sys
-if sys.version_info[0] < 3:
-	raise Exception("Python 3 not detected.")
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import svm
-from scipy import io
-
